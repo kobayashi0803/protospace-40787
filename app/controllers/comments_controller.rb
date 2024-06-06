f648e527ed3f3da5c471044708c2492c4bce79b5
kobayashi0803 <168067820+kobayashi0803@users.noreply.github.com>
2024-06-06 11:28:14 +0900

    コメント機能の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+before_action :set_prototype
+
   def create
     @comment = Comment.new(comment_params)
     @comment.user = current_user
@@ -6,12 +8,18 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@comment.prototype), notice: 'Comment was successfully created.'
     else
-      flash.now[:alert] = 'There was an error creating the comment.'
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
       render "prototypes/show"
     end
   end
 
   private
+
+  def set_prototype
+    @prototype = Prototype.find(params[:prototype_id])
+  end
+
   def comment_params
     params.require(:comment).permit(:content)
   end
